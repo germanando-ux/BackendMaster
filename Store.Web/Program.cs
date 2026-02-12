57f80f847a19a439de730dfac2b3b2491b63bf11
germanando-ux <german.anton.dopazo@gmail.com>
2026-02-12 10:05:29 +0100

    Primeros pasos en productos

--- Store.Web/Program.cs
+++ Store.Web/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Store.Web.Services;
 using Store.Web;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7260/") });
+builder.Services.AddScoped<ProductService>();
 
 await builder.Build().RunAsync();
--- Store.Web/Services/ProductService.cs
+++ Store.Web/Services/ProductService.cs
@@ -1,6 +1,42 @@
-﻿namespace Store.Web.Services
+﻿using Store.Web.Models;
+using System.Net.Http.Json;
+
+namespace Store.Web.Services
 {
     public class ProductService
     {
+        private readonly HttpClient _http;
+
+        public ProductService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        // Obtener la lista completa de productos
+        public async Task<List<Product>> GetProductsAsync()
+        {
+            return await _http.GetFromJsonAsync<List<Product>>("products") ?? new();
+        }
+
+        // Crear o actualizar producto
+        public async Task SaveProductAsync(Product product)
+        {
+            if (product.Id == 0 || product.Id == null)
+            {
+                // Si el Id es 0 o null, es un producto nuevo
+                await _http.PostAsJsonAsync("products", product);
+            }
+            else
+            {
+                // Si tiene Id, actualizamos el existente
+                await _http.PutAsJsonAsync($"products/{product.Id}", product);
+            }
+        }
+
+        // Eliminar producto
+        public async Task DeleteProductAsync(int id)
+        {
+            await _http.DeleteAsync($"products/{id}");
+        }
     }
 }
