dce0063f4e45bb76709b1da3e23a85ac20482724
germanando-ux <german.anton.dopazo@gmail.com>
2026-02-26 11:52:14 +0100

    Validaciones, control de stock en venta

--- Store.Api/Services/VentaService.cs
+++ Store.Api/Services/VentaService.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Elastic.CommonSchema;
 using Microsoft.EntityFrameworkCore;
 using Store.Data.Data;
 using Store.Data.DTO;
@@ -29,23 +30,26 @@ namespace Store.Api.Services
 
         public async Task<int> CrearVentaAsync(VentaCreateDto ventaDto)
         {
+            using var transaction = await _context.Database.BeginTransactionAsync(IsolationLevel.Serializable); // Nivel de aislamiento más estricto para evitar problemas de concurrencia
             try
             {
-                Dictionary<int, decimal> productosDb = new(); // Diccionario para almacenar los precios de los productos desde la DB
+                
+
+                Dictionary<int, Product> productosDb = new(); // Diccionario para almacenar los precios de los productos desde la DB
                 string cacheKey = "products_list"; // La llave que buscaremos en Redis
 
                 // Extraer los IDs de los productos que vienen del Front
                 var idsProductos = ventaDto.Detalles.Select(d => d.ProductId).Distinct().ToList();
 
                 // 1. Intentar recuperar de la caché a través de la Unit of Work
-                var cachedProducts = await _unitOfWork.Cache.GetAsync<IEnumerable<ProductResponseDto>>(cacheKey);              
+                var cachedProducts = await _unitOfWork.Cache.GetAsync<IEnumerable<Product>>(cacheKey);              
 
                 if (cachedProducts != null)
                 {
                     // Filtramos solo los productos que nos interesan y convertimos a diccionario
                     productosDb = cachedProducts
                         .Where(p => idsProductos.Contains(p.Id))
-                        .ToDictionary(p => p.Id, p => p.Price);
+                        .ToDictionary(p => p.Id, p => p);
 
                     // VALIDACIÓN CRÍTICA: ¿Están todos los productos?
                     if (productosDb == null || productosDb.Count != idsProductos.Count)
@@ -53,7 +57,7 @@ namespace Store.Api.Services
 
                          productosDb = await _context.Products
                             .Where(p => idsProductos.Contains(p.Id))
-                            .ToDictionaryAsync(p => p.Id, p => p.Price);
+                            .ToDictionaryAsync(p => p.Id, p => p );
                     }
                 }
                 else
@@ -61,7 +65,7 @@ namespace Store.Api.Services
 
                      productosDb = await _context.Products
                         .Where(p => idsProductos.Contains(p.Id))
-                        .ToDictionaryAsync(p => p.Id, p => p.Price);
+                        .ToDictionaryAsync(p => p.Id, p => p);
                 }
 
             
@@ -83,20 +87,35 @@ namespace Store.Api.Services
                 // Recorremos los detalles mapeados para asignar el precio real de la DB
                 foreach (var detalle in nuevaVenta.Detalles)
                 {
-                    detalle.PrecioUnitario = productosDb[detalle.ProductId];
+                    // Accedemos al producto en el diccionario
+                    var producto = productosDb[detalle.ProductId];
+
+                    detalle.PrecioUnitario = producto.Price;
+
+                    producto.Stock -= detalle.Cantidad; // Reducimos el stock del producto
+
+                    await _unitOfWork.Cache.RemoveAsync($"product:{producto.Id}");
+
+                    _context.Products.Update(producto);
                 }
+
 
                 nuevaVenta.Total = nuevaVenta.Detalles.Sum(d => d.Cantidad * d.PrecioUnitario);
 
                 // 5. Guardar con EF Core (Esto disparará el Outbox de MassTransit automáticamente)
+
                 _context.Ventas.Add(nuevaVenta);
                 await _context.SaveChangesAsync();
+                await transaction.CommitAsync();
+
+                await _unitOfWork.Cache.RemoveAsync(cacheKey);
 
                 _logger.LogInformation("Venta creada exitosamente con ID: {VentaId}", nuevaVenta.Id);
                 return nuevaVenta.Id;
             }
             catch (Exception ex)
             {
+                await transaction.RollbackAsync();
                 _logger.LogError(ex, "Error al intentar crear una venta para el cliente {Email}", ventaDto.EmailCliente);
                 throw; // Re-lanzamos la excepción para que el controlador pueda manejarla
             }
--- Store.Data/DTO/VentaCreateDto.cs
+++ Store.Data/DTO/VentaCreateDto.cs
@@ -1,4 +1,6 @@
-﻿namespace Store.Data.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Store.Data.DTO
 {
     public class VentaCreateDto
     {
@@ -6,18 +8,28 @@
         /// <summary>
         /// Correo del cliente que actúa como identificador temporal.
         /// </summary>
+
+        [Required(ErrorMessage = "El email es obligatorio")]
+        [EmailAddress(ErrorMessage = "El formato del email no es válido")]
         public string EmailCliente { get; set; } = string.Empty;
 
         /// <summary>
         /// Lista de productos seleccionados en el carrito.
-        /// </summary>
+        /// </summary>    
+
+        [Required]
+        [MinLength(1, ErrorMessage = "La venta debe tener al menos un detalle")]
         public List<VentaDetalleDto> Detalles { get; set; } = new();
 
     }
 
     public class VentaDetalleDto
     {
+        [Range(1, int.MaxValue, ErrorMessage = "El ID del producto debe ser válido")]
         public int ProductId { get; set; }
+
+        [Range(1, 1000, ErrorMessage = "La cantidad debe estar entre 1 y 1000")]
+        [Required(ErrorMessage = "La cantidad es obligatoria")]
         public int Cantidad { get; set; }
         // El precio lo recuperaremos en el Backend por seguridad 🛡️
     }
